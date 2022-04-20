17dfe2fb598fd1ba4d91a825893365f88d5b9b6e
Magzhan Istemesov <istemesov.magzhan@mail.ru>
2022-04-20 15:30:37 +0600

    add new pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Main from "./components/Main/Main";
+import Tables from "./pages/Tables";
+import Chairs from "./pages/Chairs";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Desks from "./pages/Desks";
 
 function App() {
   return (
@@ -9,7 +13,13 @@ function App() {
       
       <Nav/>
       <Header />
-      <Main />
+
+      <Chairs />
+      <Tables />
+      <Desks />
+      <About />
+      <Contacts />
+      
       <Footer />
     </div>
   );
