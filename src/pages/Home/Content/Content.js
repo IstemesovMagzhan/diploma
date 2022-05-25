4f67bea498851c927a4f301f97752895fd1e1b1f
Magzhan Istemesov <istemesov.magzhan@mail.ru>
2022-05-25 14:24:10 +0600

    add some news

--- src/pages/Home/Content/Content.js
+++ src/pages/Home/Content/Content.js
@@ -1,15 +1,14 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
-import homeImage from "../../../assets/Home.jpg";
 import classes from "./Content.module.css";
 
 
-function Content() {
+function Content({ image }) {
   return (
     <>
       <div className={classes.Content}>
-        <img src={homeImage} alt="img" />
+        <img src={image} alt="img" />
         <section>
           <div>
             <h1>Your Office. Your Style.</h1>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Main from "../Home/Main/Main";
 import Content from "./Content/Content";
+import homeImage from "../../assets/Home.jpg"
 
 
 
 function Home() {
   return (
     <>
-      <Content />
+      <Content image={homeImage}/>
 
       <Main />
     </>
