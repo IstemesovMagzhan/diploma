52f57fd422c30dd8d683a46dbf5cb90c1274d96d
Magzhan Istemesov <istemesov.magzhan@mail.ru>
2022-06-16 15:42:00 +0600

    add link

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -2,8 +2,9 @@ import Nav from "../Nav/Nav";
 import classes from "./Drawer.module.css";
 import { FiX } from "react-icons/fi";
 import { BsCart3 } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
-function Drawer({open, toggle}) {
+function Drawer({ open, toggle }) {
   const classNames = [classes.Drawer];
   if (open) {
     classNames.push(classes.open);
@@ -14,7 +15,7 @@ function Drawer({open, toggle}) {
 
   return ( 
     <div className={classNames.join(" ")}>
-      <div onClick={toggle} className={classes.backdrop}><div><a href="#"><BsCart3 /></a> <FiX /></div></div>
+      <div onClick={toggle} className={classes.backdrop}><div><Link to="/"><BsCart3 /></Link> <FiX /></div></div>
       <div onClick={toggle} className={classes.content}>
          <Nav />
       </div>
