e7b11654f8161889f866618c48a43649cc1ad831
Magzhan Istemesov <istemesov.magzhan@mail.ru>
2022-06-27 14:41:36 +0600

    Add authSlice basics

---
+++ src/redux/authSlice.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
+const key = "AIzaSyDCoBep8DiHrKiE7DsV7PIyuQjmd1swQrQ";
+const signInUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + key;
+const signUpUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + key;
+
+export const start = createAsyncThunk('auth/start', (data, thunkAPI) => {
+  // TODO
+});
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    localId: null,
+    idToken: null,
+    error: null,
+    loading: false,
+  },
+  reducers: {
+    init: (state, action) => {},
+    success: (state, action) => {},
+    loading: (state, action) => {},
+    error: (state, action) => {}
+  }
+});
+
+export default authSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import AuthSlice from "./AuthSlice";
 import cartSlice from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    auth: AuthSlice
   }
 });
 
