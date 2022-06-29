364ff76b04a72d7908da64d3af46b68d9243c3f0
Magzhan Istemesov <istemesov.magzhan@mail.ru>
2022-06-29 15:46:03 +0600

    Add stayls

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -11,18 +11,20 @@ export default function CartItem({ product, actions, quantity }) {
       <div className={classes.image}>
         <img src={product.image} alt={product.title} />
       </div>
-      <div className={classes.title}>
-        <Link to="">{product.title}</Link>
-      
-      </div>
-      <div className={classes.quantity}>
-        {actions ? <button onClick={() => dispatch(decrement(product.productId))}>-</button> : null}
-        {quantity}
-        {actions ? <button onClick={() => dispatch(increment(product.productId))}>+</button> : null}
-      </div>
-      <div className={classes.subtotal}>
-        ${product.price * quantity}
-        {actions ? <button onClick={() => dispatch(remove(product.productId))}>Delete</button> : null}
+      <div>
+       <div className={classes.title}>
+          <Link to="">{product.title}</Link>
+        
+        </div>
+        <div className={classes.quantity}>
+          {actions ? <button onClick={() => dispatch(decrement(product.productId))}>-</button> : null}
+          {quantity}
+          {actions ? <button onClick={() => dispatch(increment(product.productId))}>+</button> : null}
+        </div>
+        <div className={classes.subtotal}>
+          ${product.price * quantity}
+          {actions ? <button onClick={() => dispatch(remove(product.productId))}>Delete</button> : null}
+        </div>
       </div>
     </div>
   )
--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import classes from "./CartLink.module.css";
+import { BsCart3 } from "react-icons/bs";
 
 export default function CartLink() {
   const number = useSelector(store => {
@@ -7,6 +9,6 @@ export default function CartLink() {
   });
 
   return (
-    <NavLink to="/cart"> Cart ({number})</NavLink>
+    <NavLink to="/cart" className={classes.CartLink}> <BsCart3 className="BsCart3" /> ({number})</NavLink>
   );
 }
